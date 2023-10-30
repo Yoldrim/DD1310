19a20c6d5ad7040d8bbaa7f419cb12b76faf8ba2
Harald Bergqvist <harald.bergqvist@gmail.com>
2023-10-30 09:38:08 +0100

    lab5

--- lab5/del1/simulator.py
+++ lab5/del1/simulator.py
@@ -1,5 +1,6 @@
 from TV import TV
 
+FILE_NAME = 'db.txt'
 
 def read_file(file):
     tvs = []
@@ -19,15 +20,71 @@ def write_file(tv_list, file):
     f.write(out_str)
 
 
-tvlista = read_file("db.txt")
-for tv in tvlista: print(tv)     # Här ska programmet skriva namn, inställd kanal
-                                 # och ljudnivå för båda tv-apparaten som finns i filen
+def number_input(prompt, min, max):
+    try:
+        inp = int(input(prompt))
+    except ValueError:
+        print('Du måste skriva in ett tal, försök igen.')
+        return number_input(prompt, min, max)
 
-tvlista[0].change_channel(5)
-tvlista[1].increase_volume()
-for tv in tvlista: print(tv)     # Här ska programmet skriva ut information
-                                 # om båda tv-apparaterna (Observera att kanalen för
-                                 # första tv:n måste ha ändrats till 1)
-write_file(tvlista, "db.txt")
-tvlista = read_file("db.txt")
-for tv in tvlista: print(tv)
+    if inp >= min and inp <= max:
+        return inp
+    else:
+        print(f'Du måste skriva in ett nummer i spannet {min}-{max}, försök igen!')
+        return number_input(prompt, min, max)
+
+
+def change_channel(tv_instance):
+    inp = number_input('Välj kanal: ', 0, tv_instance.max_channels)
+    if not tv_instance.change_channel(inp):
+        print(f'Kunde inte ändra kanal till {inp}. Försök igen.')
+
+
+def increase_volume(tv_instance):
+    if not tv_instance.increase_volume():
+        print('Volymen är på max!')
+
+
+def decrease_volume(tv_instance):
+    if not tv_instance.decrease_vollume():
+        print('Volymen är på 0.')
+
+
+def adjust_TV_menu(tv_instance):
+    print(f'\n{tv_instance.tv_name}')
+    print(f'Kanal: {tv_instance.current_channel}')
+    print(f'Volym: {tv_instance.current_volume}')
+
+    print('\n1. Byt kanal')
+    print('2. Höj volymen')
+    print('3. Sänk volymen')
+    print('4. Gå tillbaks till huvudmenyn')
+
+    return number_input('Välj: ', 1, 4)
+
+def select_TV_menu(tvs):
+    while True:
+        for i in range(len(tvs)):
+            print(f'{i+1}. {tvs[i].tv_name}')
+        print(f'{len(tvs)+1}. Avsluta')
+        inp = number_input('Välj: ', 1, len(tvs)+1)
+        if inp == len(tvs)+1:
+            write_file(tvs, FILE_NAME)
+            print('Simulering avslutad.')
+            break
+        else:
+            active_tv = tvs[inp-1]
+            while True:
+                choice = adjust_TV_menu(active_tv)
+                if choice == 1:
+                    change_channel(active_tv)
+                elif choice == 2:
+                    increase_volume(active_tv)
+                elif choice == 3:
+                    decrease_volume(active_tv)
+                else:
+                    break
+
+
+tvlista = read_file(FILE_NAME)
+select_TV_menu(tvlista)
